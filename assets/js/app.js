ba55e9afddbfc3c2885f02c9dce2c7fa86c9da28
Maksumi Murakami <maksumimurakami@gmail.com>
2023-10-04 18:10:32 -0600

    Avance del ejercicio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,24 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+async function displayUser(username) {
+   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
+
+//Nota para Felipe: Este ejercicio esta en proceso, estoy investigando mas sobre el manejo de las promesas ya que estoy atorado. Si tienes algun recurso será bienvenido. Gracisa!
